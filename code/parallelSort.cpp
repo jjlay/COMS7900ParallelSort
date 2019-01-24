618cd2571b7a23e5d677100ce71714cb25c04110
JJ Lay <jj.lay@tmcat.com>
2019-01-23 22:33:06 -0600

    Tweaked the Makefile and tested MPI

--- code/parallelSort.cpp
+++ code/parallelSort.cpp
@@ -2,17 +2,49 @@
 // Parallel Sort
 //
 
+//
+// Standard includes
+//
+
 #include <iostream>
 #include <iomanip>
 
+//
+// Parallel includes
+//
+
+#include "mpi.h"
+
+
+//
+// Local includes
+//
 
 #include "definitions.h"
 
 
+//
+// Function: main
+//
+
 int main(int argc, char *argv[])
 {
+	//
+	// MPI setup
+	//
+	
+	MPI_Init(&argc, &argv);
+
 	std::cout << "Team Metropolis Parallel Sort" << std::endl;
 
+
+
+
+	//
+	// Wrap up
+	//
+	
+	MPI_Finalize();
 
 	return _OKAY_;
 }
