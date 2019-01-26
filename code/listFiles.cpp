495851babacfdc2313000910c4ea826ec7e2acf1
JJ Lay <jj.lay@tmcat.com>
2019-01-26 15:46:09 -0600

    Created listFiles function

---
+++ code/listFiles.cpp
@@ -0,0 +1,30 @@
+//
+// listFiles
+//
+
+//
+// STL includes
+//
+
+#include <vector>
+
+//
+// Standard includes
+//
+
+#include <string>
+
+
+//
+// Function: listFiles
+//
+
+std::vector<std::string> listFiles(std::string path) {
+	std::vector<std::string> files;
+
+
+
+	return files;
+}
+
+
