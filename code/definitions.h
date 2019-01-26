280805fb89e3b49f7453096cbc05ee58bb815af7
JJ Lay <jj.lay@tmcat.com>
2019-01-25 21:40:28 -0600

    Skeleton of MPI code

--- code/definitions.h
+++ code/definitions.h
@@ -9,3 +9,19 @@
 #define _OKAY_   0
 
 
+//
+// Node Status
+//
+
+#define NodeUndefined  -1
+#define NodeExit       -3
+
+
+//
+// MPI Tags
+//
+
+#define mpi_Tag_Name    10
+#define mpi_Tag_State   20
+
+
