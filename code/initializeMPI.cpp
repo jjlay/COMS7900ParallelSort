90a17a36f9ff535cbc2ecbffe042732c24fb2606
JJ Lay <jj.lay@tmcat.com>
2019-01-25 22:05:07 -0600

    MPI initialization code

---
+++ code/initializeMPI.cpp
@@ -0,0 +1,57 @@
+//
+// Initialize MPI
+//
+
+//
+// Standard includes
+//
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+
+//
+// Parallel includes
+//
+
+#include "mpi.h"
+
+
+//
+// Local includes
+//
+
+#include "definitions.h"
+
+
+//
+// Function: initializeMPI
+//
+
+void initializeMPI(std::string *processorName, int *myRank, int *numNodes, int argc, char *argv[])
+{
+	//
+	// MPI setup
+	//
+	
+	MPI_Init(&argc, &argv);
+
+
+	auto localProcName = new char[MPI_MAX_PROCESSOR_NAME];
+	int localNumProcs, localMyRank, localNameLen;
+
+	MPI_Comm_size(MPI_COMM_WORLD, &localNumProcs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &localMyRank);
+	MPI_Get_processor_name(localProcName, &localNameLen);
+
+	*myRank = localMyRank;
+	*numNodes = localNumProcs;
+	*processorName = std::string(localProcName);
+
+#ifdef _DEBUG_
+	std::cout << "Rank " << myRank << " out of " << numProcs << " nodes" 
+		<< " on Hostname: " << procName << std::endl;
+#endif
+
+}
