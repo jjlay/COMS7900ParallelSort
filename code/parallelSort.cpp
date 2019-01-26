9c14771ea457e19d7ec9e0940b6c9736912c93bc
JJ Lay <jj.lay@tmcat.com>
2019-01-25 21:43:03 -0600

    Skeleton of MPI code

--- code/parallelSort.cpp
+++ code/parallelSort.cpp
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
 	
 	MPI_Init(&argc, &argv);
 
-	std::cout << "Parts Forecasting" << std::endl;
+	std::cout << "Team Metropolis Parallel Sort" << std::endl;
 
 	int numProcs, myRank, nameLen;
 
@@ -57,67 +57,7 @@ int main(int argc, char *argv[])
 	std::cout << "Rank " << myRank << " out of " << numProcs << " nodes" 
 		<< " on Hostname: " << procName << std::endl;
 
-	std::map<int, std::string> nodeList;
-	std::map<std::string, int> nameCount;
-	auto nodeState = new int[numProcs];
-	int myState = NodeUndefined;
 
-
-	if (myRank == 0) {
-		std::string s(procName);
-		nodeList[0] = s;
-
-		// We need to collect the nodes
-		auto tempName = new char[MPI_MAX_PROCESSOR_NAME];
-		MPI_Status tempStatus;
-
-		for (auto n = 1; n < numProcs; n++) {
-			MPI_Recv(tempName, MPI_MAX_PROCESSOR_NAME, MPI_CHAR, n, mpi_Tag_Name, MPI_COMM_WORLD, &tempStatus);
-			s.assign(tempName);
-			nodeList[n] = s;
-		}
-	} else {
-		// Send my node information
-		MPI_Send(procName, MPI_MAX_PROCESSOR_NAME, MPI_CHAR, 0, mpi_Tag_Name, MPI_COMM_WORLD);
-	}
-
-	// Decide which nodes we will let work and which ones will exit
-	if (myRank == 0) {
-		for (auto n = 0; n < numProcs; n++)
-			nodeState[n] = NodeUndefined;
-
-		for (auto n = 0; n < numProcs; n++) {
-			std::cout << "Node " <<  n << " : " << nodeList[n] << std::endl;
-
-			if (nameCount.find(nodeList[n]) == nameCount.end()) {
-				// This node has not been seen yet
-				nameCount[nodeList[n]] = 1;
-
-				nodeState[n] = 0;
-			} else {
-				nameCount[nodeList[n]]++;
-
-				if (nameCount[nodeList[n]] == 2)
-					nodeState[n] = 1;
-				else 
-					nodeState[n] = NodeExit;
-			}
-		}
-
-		for (auto n : nameCount)
-			std::cout << n.first << " : " << n.second << std::endl;
-
-		for (auto n = 0; n < numProcs; n++) {
-			std::cout << "Node " << n << " state " << nodeState[n] << std::endl;
-			MPI_Send(&nodeState[n], 1, MPI_INT, n, mpi_Tag_State, MPI_COMM_WORLD);
-		}
-	} 
-	else {
-		MPI_Status tempStatus;
-		MPI_Recv(&myState, 1, MPI_INT, 0, mpi_Tag_State, MPI_COMM_WORLD, &tempStatus);
-
-		std::cout << "Node " << myRank << " and my state is " << myState << std::endl;
-	}
 
 
 	//
