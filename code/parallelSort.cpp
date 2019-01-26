79f55e402fef3f2db37ae20b90127aeee4f783bd
JJ Lay <jj.lay@tmcat.com>
2019-01-25 21:58:13 -0600

    MPI initialization code

--- code/parallelSort.cpp
+++ code/parallelSort.cpp
@@ -19,17 +19,11 @@
 
 
 //
-// STL includes
-//
-
-#include <map>
-
-
-//
 // Local includes
 //
 
 #include "definitions.h"
+#include "initializeMPI.h"
 
 
 //
@@ -41,21 +35,16 @@ int main(int argc, char *argv[])
 	//
 	// MPI setup
 	//
-	
-	MPI_Init(&argc, &argv);
 
-	std::cout << "Team Metropolis Parallel Sort" << std::endl;
+	std::string processorName;
+	int myRank, numNodes;
 
-	int numProcs, myRank, nameLen;
+	initializeMPI(&processorName, &myRank, &numNodes, argc, argv);
 
-	auto procName = new char[MPI_MAX_PROCESSOR_NAME];
-
-	MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-	MPI_Get_processor_name(procName, &nameLen);
-
-	std::cout << "Rank " << myRank << " out of " << numProcs << " nodes" 
-		<< " on Hostname: " << procName << std::endl;
+	std::cout << "Rank " << myRank
+		<< " running on " << processorName 
+		<< " with " << numNodes << " total processes" 
+		<< std::endl;
 
 
 
