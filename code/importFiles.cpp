18d789f3b388128fa9b7b4534d6b6a154c8bb174
JJ Lay <jj.lay@tmcat.com>
2019-01-26 23:16:49 -0600

    Placeholder for importFiles

---
+++ code/importFiles.cpp
@@ -0,0 +1,32 @@
+//
+// importFiles
+//
+
+
+#undef _DEBUG_
+
+
+//
+// STL includes
+//
+
+#include <vector>
+
+
+//
+// Standard includes
+//
+
+#include <string>
+
+
+//
+// Function: importFiles
+//
+
+double *importFiles(std::vector<std::string> files) {
+
+
+
+	return nullptr;
+}
