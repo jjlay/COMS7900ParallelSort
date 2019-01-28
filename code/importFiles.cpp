4c0ed04d4a9dc3a04e4c292fe9748ff5dbaeed62
gtw2i <gtw2i@mtmail.mtsu.edu>
2019-01-28 12:04:49 -0600

    parsed lines

--- code/importFiles.cpp
+++ code/importFiles.cpp
@@ -39,26 +39,31 @@ double *importFiles(std::vector<std::string> files, int myRank) {
 	Data_COMS data[numLines]; // compiler directive (see definitions.h)
 	
 	// loop through files to read
-	for ( auto f:files ){
+	for ( auto f:files ) {
 		std::ifstream infile( f );
 		std::string line;
+		std::string delimiter = " ";
+		std::string token;
+		size_t pos = 0;
 		
 		// loop through lines of file
-		while( std::getline(infile, line) and count <= numLines ){
-			std::cout << line << std::endl;
+		while( std::getline(infile, line) and count <= numLines ) {
+		//	std::cout << line << std::endl;
+			
+			while( (pos = line.find(delimiter)) != std::string::npos ) {
+				
+				token = line.substr(0, pos);
+				std::cout << token << std::endl;
+				line.erase(0, pos + delimiter.length());
+				
+			}
+			
 			
 		}
 		
 		
+		// close the file
 	}
-		
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	return nullptr;
