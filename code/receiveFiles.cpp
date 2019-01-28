893eda59891469fb911f4d9870e61920484a12a3
JJ Lay <jj.lay@tmcat.com>
2019-01-27 22:44:14 -0600

    Added comments to receiveFiles

--- code/receiveFiles.cpp
+++ code/receiveFiles.cpp
@@ -39,9 +39,17 @@
 //
 // Function: receiveFiles
 //
+// Parameters:
+//   myRank - the rank of the current process
+//
+// Returns:
+//   A vector of strings containing the filenames
+//   for this process to work on.
+//
 
 std::vector<std::string> receiveFiles(int myRank) {
 
+	// The vector to hold the filenames
 	std::vector<std::string> files;
 
 	int mpiReturn;
@@ -54,17 +62,25 @@ std::vector<std::string> receiveFiles(int myRank) {
 		<< std::endl;
 #endif
 
+	// Allocate memory for the buffer
 	char *buffer = static_cast<char *>(calloc(mpi_Max_Filename * 2, sizeof(char)));
+
+	// This is the expected string to signal that rank 0
+	// is done sending files.
 	std::string strDone = "DONE!";
 
 	int Run = 1;
 
 	while(Run) {
+		// Look for a filename
 		mpiReturn = MPI_Irecv(buffer, mpi_Max_Filename, MPI_BYTE,
 			0, mpi_Tag_File, MPI_COMM_WORLD, &request);
 
+		// Wait until one arrives since we do
+		// not have anything else to do.
 		MPI_Wait(&request, &status);
 
+		// Convert the filename to a C++ string
 		auto s = std::string(buffer);
 
 #ifdef _DEBUG_
@@ -73,11 +89,12 @@ std::vector<std::string> receiveFiles(int myRank) {
 			<< std::endl;
 #endif
 
-
+		// Have we received the signal to end?
 		if (s == strDone) {
 			Run = 0;
 		}
 		else {
+			// If this is a filename, add it to our list.
 			files.push_back(s);
 		}
 	}
