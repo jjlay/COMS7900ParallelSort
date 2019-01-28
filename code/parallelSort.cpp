e02c37849651ce0caf5697df9747b09ad6b63e40
JJ Lay <jj.lay@tmcat.com>
2019-01-27 22:24:18 -0600

    Bug with sending wrong rank to receiveFiles

--- code/parallelSort.cpp
+++ code/parallelSort.cpp
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 	} 
 	else {
 		// Receive file list
-		FilenameArray = receiveFiles(numNodes-1);
+		FilenameArray = receiveFiles(myRank);
 	}
 
 	if (myRank != 0) {
