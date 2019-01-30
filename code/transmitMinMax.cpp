0d3c78c06e3fe2a21abd87a7a147bf458226d5c1
JJ Lay <jj.lay@tmcat.com>
2019-01-30 10:51:17 -0600

    Working on transmitMinMax

--- code/transmitMinMax.cpp
+++ code/transmitMinMax.cpp
@@ -22,7 +22,13 @@ void transmitMinMax(double min, double max) {
 	minMax[_MIN_] = min;
 	minMax[_MAX_] = max;
 
-	int result = MPI_Send(
+	int result = MPI_Send(minMax, 2, MPI_DOUBLE, Rank0,
+		mpi_Tag_SendMinMax, MPI_COMM_WORLD);
+	
+	char *error;
+	int errorLen;
+	MPI_Error_string(result, error, &errorLen);
+	std::cerr << "MPI Result " << result << " : " << error << std::endl;
 	
 
 }
