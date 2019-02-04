e5748f776ac0861b032372456752e6e8bc9bd3cd
JJ Lay <jj.lay@tmcat.com>
2019-02-03 22:12:21 -0600

    Forgot a file

---
+++ code/receiveMinMax.cpp
@@ -0,0 +1,52 @@
+//
+// Local includes
+//
+
+#include "definitions.h"
+
+
+//
+// Standard includes
+//
+
+#include <iostream>
+
+
+//
+// Parallel includes
+//
+
+#include "mpi.h"
+
+
+//
+// Function: receiveMinMax
+//
+// Parameters:
+//   min - least value
+//   max - greatest value
+//
+// Returns:
+//   Nothing
+
+void receiveMinMax(double *min, double *max) {
+
+	MPI_Status status;
+	auto minMax = new double[2];
+
+	minMax[_MIN_] = 0.0;
+	minMax[_MAX_] = 0.0;
+
+	int result = MPI_Recv(&minMax, 2, MPI_DOUBLE, Rank0,
+		mpi_Tag_SendMinMax, MPI_COMM_WORLD, &status);
+	
+	char *error;
+	int errorLen;
+	MPI_Error_string(result, error, &errorLen);
+	std::cerr << "MinMax MPI received " << result << " : " << error << std::endl;
+	
+	*min = minMax[_MIN_];
+	*max = minMax[_MAX_];
+}
+
+
---
+++ code/receiveMinMax.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//
+// Function: receiveMinMax
+//
+// Parameters:
+//   min - least value
+//   max - greatest value
+//
+// Returns:
+//   Nothing
+
+void receiveMinMax(double *min, double *max);
+
+
