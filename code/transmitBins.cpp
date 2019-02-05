fdf64174b83e1a67152a01a36bef5238321816f8
JJ Lay <hal2a@mtmail.mtsu.edu>
2019-02-04 22:20:30 -0600

    Placeholders for transmitBins

--- code/transmitBins.cpp
+++ code/transmitBins.cpp
@@ -1,6 +1,30 @@
+//
+// Standard includes
+//
+
+#include <iostream>
+#include <iomanip>
 
 
-void transmitBins() {
+//
+// Local includes
+//
 
+#include "definitions.h"
+
+
+//
+// Function: transmitBins
+//
+// Parameters:
+//    bins - pointer to the array of bin edges
+//
+//    numWorkers - number of workers
+
+void transmitBins(double *bins, int numWorkers) {
+
+	std::cout << "Transmitting bins" << std::endl;
 
 }
+
+
---
+++ code/transmitBins.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+//
+// Function: transmitBins
+//
+// Parameters:
+//    bins - pointer to the array of bin edges
+//
+//    numWorkers - number of workers
+
+void transmitBins(double *bins, int numWorkers);
+
+
